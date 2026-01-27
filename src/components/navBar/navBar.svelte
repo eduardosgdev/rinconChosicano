6d356e732b2709c5eb23168bb403a99059bc0ed2
eduardoscdev <230696105+eduardosgdev@users.noreply.github.com>
2026-01-27 19:30:35 +0100

    rincon 1

---
+++ src/components/navBar/navBar.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { goto } from "$app/navigation";
+    import "../../app.css"
+    let isOpen = false;
+    const logo = "/img/logo/logo.png    "
+  
+    function toggleMenu() {
+      isOpen = !isOpen;
+    }
+  </script>
+  <main class=" text-white px-2 py-2 bg-red-600 ">
+    <!-- Contenedor principal de la navbar -->
+    <nav class="flex items-center justify-between">
+        <img src={logo} alt="logo" class="w-36 h-24 object-scale-cover">
+      <!-- Menú hamburguesa -->
+        <button class="p-1 text-white hover:bg-gray-800 rounded" on:click={toggleMenu}>
+          <!-- Ícono de menú (hamburguesa) -->
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>      
+    </nav>
+  
+    <!-- Menú desplegable en dispositivos móviles -->
+    <div class={`lg:hidden ${isOpen ? 'block' : 'hidden'}  text-white  ml-2 text-[16px] font-semibold`}>
+      <a href="/" class="block py-2 hover:bg-gray-800">Inicio</a>
+      <a href="/escaparate" class="block py-2 hover:bg-gray-800">Escaparate</a>  
+    </div>
+    
+  </main>
+  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    import NavBar from "../components/navBar/navBar.svelte";
+</script>
+<main>
+    <NavBar />
+    <section
+  class="relative p-2 min-h-screen bg-cover bg-center"
+  style="background-image: url('/img/cristo.jpg');"
+>
+  <!-- overlay oscuro SOLO para el fondo -->
+  <div class="absolute inset-1 bg-black/50"></div>
+
+  <!-- contenido encima -->
+  <div class="relative z-10 h-[90vh] flex flex-col justify-center mt-12">
+    <div class="mb-4">
+      <h1 class="p-2 font-bold text-[45px] text-white">
+        EL MEJOR RINCÓN PARA CONSENTIR EL PALADAR
+      </h1>
+
+      <p class="p-2 font-bold text-gray-200 text-[12px] max-w-xl">
+        En el rincón chosicano trabajamos cada día para brindarte una experiencia
+        llena de sabor y tradición, combinando años de experiencia de nuestros chefs
+        con el exquisito toque norteño que potencia los sabores
+      </p>
+    </div>
+
+    <div class="flex justify-around mt-8">
+      <button class="bg-white px-6 py-2 text-red-600 rounded-xl font-bold">
+        Ver carta
+      </button>
+      <button class="bg-white px-6 py-2 text-red-600 rounded-xl font-bold">
+        Contactar
+      </button>
+    </div>
+  </div>
+</section>
+    <section>
+        <h2>Hola</h2>
+    </section>
+    
+
+</main>
+
